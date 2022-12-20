daf9858a91200665fd4a038a3d4c424c8db0d973
Carla Willmes <willmes.carla@gmail.com>
2022-12-20 12:32:00 +0900

    styled header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='header--left'>
+        <img src='images/header-logo.png' alt='' className='header--img' />
+        <h3>Meme Generator</h3>
+      </div>
+      <p>Built with love 💖</p>
+    </div>
+  )
+}
