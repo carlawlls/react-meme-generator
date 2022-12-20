1d0ab3b619a62a8519a74c7570c644295ee285cf
Carla Willmes <willmes.carla@gmail.com>
2022-12-20 13:10:45 +0900

    created meme component

---
+++ src/components/Meme.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Meme() {
+  return (
+    <div className='meme'>
+      <input></input>
+      <input></input>
+      <button>Get a new meme image 🖼️</button>
+    </div>
+  )
+}
